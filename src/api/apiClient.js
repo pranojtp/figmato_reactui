d3e010202260971064a2ccadcf893b99cb6d924c
Pranoj <tppranoj22@gmail.com>
2025-09-14 19:32:46 +0530

    third commit after read VITE_API_URL from env;

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: '/api',
+  baseURL:
+    import.meta.env.MODE === 'development'
+      ? '/api'
+      : import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
